6e985fa32528fd0178013dcccf69fb7edcc1f351
rexxh <rexxh@mail.ru>
2016-11-20 16:02:41 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -88,13 +88,18 @@ allocator<T>::allocator(size_t size) : ptr_(static_cast<T *>(size == 0 ? nullptr
 
 template<typename T>
 allocator<T>::allocator(allocator const& other) : allocator<T>(other.size_) {
-	//allocator<T> temp (other.size_);
+	allocator<T> temp (other.size_);
 	//for (size_t i=0; i < temp.size_; i++) {
 	//	construct(temp.ptr_ + i, other.ptr_[i]); 
 	//}
 	//this->swap(temp);
+	try {
 	for (size_t i= 0; i < size_; i++) {
 		construct(ptr_ + i, other.ptr_[i]); }
+	}
+	catch(...)
+	{
+		
 }
 
 template<typename T>
