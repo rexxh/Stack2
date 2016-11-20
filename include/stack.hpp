a2b7fa42a9c3f7d0872037de583fd0478d901101
rexxh <rexxh@mail.ru>
2016-11-20 15:33:04 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -87,7 +87,7 @@ template<typename T>
 allocator<T>::allocator(size_t size) : ptr_(static_cast<T *>(size == 0 ? nullptr : operator new(size * sizeof(T)))), size_(size), map_(std::make_unique<bitset>(size)) {}
 
 template<typename T>
-allocator<T>::allocator(allocator const& other) : allocator<T>(other.size_); {
+allocator<T>::allocator(allocator const& other) : allocator<T>(other.size_) {
 	allocator<T> temp (other.size_);
 	for (size_t i=0; i < temp.size_; i++) {
 		construct(temp.ptr_ + i, other.ptr_[i]); 
